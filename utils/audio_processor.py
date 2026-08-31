3f70a8a30603c846cb76e04d24cee4e1464ab73e
Ayush Tayal <ayushtayal685@gmail.com>
2026-08-31 17:17:42 +0530

    new

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -11,7 +11,7 @@ def download_youtube_audio(url: str) -> str:
     ydl_opts = {
         # Prefer the widely available combined format.
         # FFmpeg will extract the audio afterward.
-        "format": "bv*+ba/b",
+        "format": "ba/b",
         "outtmpl": output_path,
 
         "postprocessors": [
